b32a7a101e62844ff349667dacf5c1882d32ea79
Conor Geraghty <cg2028@bath.ac.uk>
2021-12-14 19:17:24 +0000

    Add method to randomize first turn. Also added break to if statement for board full.

--- MyConnectFour.java
+++ MyConnectFour.java
@@ -9,6 +9,7 @@ public class MyConnectFour {
     Player currentPlayer;
     Piece currentPlayerPiece;
     Player winner = null;
+    int toReturn;
     int columnChoice = 1;
     int turnsTaken = 0;
     int rowPos = -1;
@@ -23,22 +24,22 @@ public class MyConnectFour {
         players = new Player[2];
         players[0] = new Player(1, Piece.RED);
         players[1] = new Player(2, Piece.YELLOW);
-        currentPlayer = players[0];
-
 
         playGame();
 
     }
 
     private void playGame() {
-        System.out.println("");
-        System.out.println("Welcome to Connect 4");
+
+        System.out.println("\n Welcome to Connect 4");
         System.out.println("There are 2 players red and yellow");
         System.out.println("Player 1 is Red, Player 2 is Yellow");
         System.out.println("To play the game type in the number of the column you want to drop you counter in");
-        System.out.println("A player wins by connecting 4 counters in a row - vertically, horizontally or diagonally");
-        System.out.println("");
+        System.out.println("A player wins by connecting 4 counters in a row - vertically, horizontally or diagonally. \n");
+
         board.printGame();
+
+        randomizeFirstTurn();
 
         while (true) {
 
@@ -52,6 +53,7 @@ public class MyConnectFour {
                 if (turnsTaken == 42) {
 
                     System.out.println("Board is full. GAME OVER.");
+                    break;
 
                 }
 
@@ -132,7 +134,7 @@ public class MyConnectFour {
 
     private int getUserInput() {
 
-        int toReturn;
+
 
         try {
 
@@ -147,6 +149,25 @@ public class MyConnectFour {
         }
 
         return toReturn;
+    }
+
+    private void randomizeFirstTurn(){
+
+        if(rand.nextInt(10) < 5){
+
+            currentPlayer = players[0];
+            System.out.println("\nHuman Player goes first. ");
+
+        }
+
+        else{
+
+            currentPlayer = players[1];
+            System.out.println("\nRobot Player goes first. ");
+
+        }
+
+
     }
 
 
