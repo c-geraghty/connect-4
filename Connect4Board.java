68616644a57313ba9099adb9b2f7db41e536dae6
Conor Geraghty <cg2028@bath.ac.uk>
2021-12-15 14:15:00 +0000

    Extend board class to connect4board. Allows the board class to be used for other games if needed.

---
+++ Connect4Board.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class Connect4Board extends Board{
+
+    public Connect4Board(int col, int row) {
+
+        super(col, row);
+
+    }
+
+    public int checkIfPieceAccepted(Piece piece, int colChoice){
+
+
+
+        for(int i = row-1; i >= 0; i--){
+
+            if(board[i][colChoice] == Piece.BLANK){
+
+                System.out.println(" ");
+                System.out.println("Piece Placed Successfully. ");
+                System.out.println(" ");
+                board[i][colChoice] = piece;
+
+                printGame();
+                return i;
+
+
+            }
+
+        }
+
+
+        System.out.println("\nPiece Not Placed. ");
+        System.out.println("Column is full. ");
+
+
+
+        return -1;
+
+    }
+
+    public Piece getPiece(int rowPos, int colPos){
+
+        return currentPiece = board[rowPos][colPos];
+
+    }
+
+}
